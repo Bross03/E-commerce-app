53a16c2fd9fa79eebfc4656cdd9af474cff59ea1
Joao Pedro Brossel <jpbrossel@hotmail.com>
2022-06-21 01:23:20 -0400

    install provider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import { BrowserRouter } from 'react-router-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './store/rootReducer';
+import { Provider } from 'react-redux';
 
 const store=configureStore({
   reducer:rootReducer
